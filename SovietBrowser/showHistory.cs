1af94965b57db23eb8caf28a3d535f4be159bfeb
Stefano Schoonheim <s.schoonheim@elevate.nl>
2019-11-29 15:59:49 +0100

    Added some comments to explain whats happening

--- SovietBrowser/showHistory.cs
+++ SovietBrowser/showHistory.cs
@@ -13,16 +13,25 @@ namespace SovietBrowser {
   public partial class showHistory : Form {
     public showHistory() {
       InitializeComponent();
+      
       var path = (@"C:\Users\Stefano\AppData\Local\EzClapsBrowser\History.txt");
+      //looks if the history.txt file exists
       if (!File.Exists(path)) {
+        //path doesnt exist yet so we create one uwu
         string root = @"C:\Users\Stefano\AppData\Local\EzClapsBrowser";
+        //Looks up if the Folder exists 
         if (!Directory.Exists(root)) {
+          //doesnt exist so we make one based on the root directory
           Directory.CreateDirectory(root);
+          //we also make a new History.txt file ( if the directory doesnt exist then the file wouldnt exist aswel ) 
           File.Create(path);
         }
       } else {
+        //if both the directory and History.txt exist or been made then we can enter the folder and read / 
+        //write out the file and whats inside
         string content = File.ReadAllText(path);
         txtHistory.Text = content;
+        
       }
     }
   }
