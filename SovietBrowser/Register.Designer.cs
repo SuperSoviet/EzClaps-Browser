bbde75b3fa1230b77c17e29aef21c8e4754d9d4f
Stefano Schoonheim <s.schoonheim@elevate.nl>
2019-12-05 15:01:13 +0100

    *added* new icons for login and register

--- SovietBrowser/Register.Designer.cs
+++ SovietBrowser/Register.Designer.cs
@@ -23,6 +23,7 @@
     /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
       this.SuspendLayout();
       // 
       // Register
@@ -30,6 +31,7 @@
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 828);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
       this.Name = "Register";
       this.Text = "Register";
       this.ResumeLayout(false);
