b283b6420fef22b76a8756a9fb2f464f93a70cb9
Stefano Schoonheim <s.schoonheim@elevate.nl>
2020-01-02 09:57:01 +0100

    Changed // tekst

--- SovietBrowser/Login.cs
+++ SovietBrowser/Login.cs
@@ -21,7 +21,8 @@ namespace EzClapsBrowser {
     string[] passwords = { "100%Koffie", "bubugamer", "Pa130576" };
 
     private void btnLogin_Click(object sender, EventArgs e) {
-      //the moment you click the button login it checks if your logins are correct
+      //the moment you click the button login it checks if your logins are correct 
+      //by checking the textboxes to see if the text you put in is the same as the tekst of the string array ( for both username and password ) 
       if (usernames.Contains(txtUsername.Text)
         && passwords.Contains(txtPassword.Text)
         && Array.IndexOf(usernames, txtUsername.Text) ==
@@ -32,7 +33,7 @@ namespace EzClapsBrowser {
         browserForm.ShowDialog();
 
       } else {
-        //otherwise it opens a MessageBox telling you its the wrong login
+        //otherwise it opens a MessageBox telling you its the wrong login and showing you how to get a login or change your login
         MessageBox.Show("Wrong login, if you wish to reset it or get a login go to my Discord EzClaps Den and contact me. if your not yet in my discord use the Register button to do so.");
         
 
