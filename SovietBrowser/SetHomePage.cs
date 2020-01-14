a414d768e412e7cec44c5a8b95fdbd44bc83983a
Stefano Schoonheim <s.schoonheim@elevate.nl>
2020-01-14 10:41:33 +0100

    *added* fully working set home page tab

--- SovietBrowser/SetHomePage.cs
+++ SovietBrowser/SetHomePage.cs
@@ -6,6 +6,7 @@ namespace EzClapsBrowser {
   public partial class SetHomePage : Form {
     public SetHomePage() {
       InitializeComponent();
+
       string root = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\EzclapsBrowser";
       var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\EzclapsBrowser\Home.txt";
       if (!Directory.Exists(root)) {
@@ -17,16 +18,21 @@ namespace EzClapsBrowser {
         txtHomePage.Text = content;
       }
     }
-
-    private void btnSaveHome_Click(object sender, EventArgs e) {
-      var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\EzclapsBrowser\Home.txt";
-      
-      File.WriteAllText(txtHomePage.Text, path);
-    }
-
+    //Loads in the path > then what is inside the textfile > puts the text of the file into the textbox of the winform
     private void SetHomePage_Load(object sender, EventArgs e) {
       var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\EzclapsBrowser\Home.txt";
       File.ReadAllText(path);
+    }
+
+
+    private void btnSaveHome_Click(object sender, EventArgs e) {
+      //when you press safe it searches for the file location 
+      //then it will remove everything in the text file to make sure it is clean 
+      //and it doesnt overlap with old text on accident
+      //after that it will add the text that you put in the text box and use it as a homepage
+      var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\EzclapsBrowser\Home.txt";
+      File.WriteAllText(path, "");
+      File.WriteAllText(path, txtHomePage.Text);
     }
   }
 }
